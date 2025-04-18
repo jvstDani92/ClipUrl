342f10d8bb0137cf9cbfffd4bbe04dea9dfc7ba3
jvstDani92 <radomirovDaniel@gmail.com>
2025-04-18 19:56:28 +0200

    feat(domain): add repository interface

---
+++ ClipUrl.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace ClipUrl.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<IEnumerable<T>> GetAllWithIncludesAsync(params Expression<Func<T, object>>[] includes);
+
+        Task<T?> GetByIdAsync(Guid id);
+
+        Task<T?> GetByIdWithIncludesAsync(Guid id, params Expression<Func<T, object>>[] includes);
+
+        Task AddAsync(T entity);
+
+        void UpdateAsync(T entity);
+
+        void DeleteAsync(T entity);
+
+        Task SaveChangesAsync();
+    }
+}
