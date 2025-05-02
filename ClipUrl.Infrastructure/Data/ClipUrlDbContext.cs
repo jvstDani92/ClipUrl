87e2e75043d2e133ba38f0d3ef977ee32dffcf12
jvstDani92 <radomirovDaniel@gmail.com>
2025-05-02 08:18:57 +0200

    feat(infrastructure): add dbContext

---
+++ ClipUrl.Infrastructure/Data/ClipUrlDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ClipUrl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClipUrl.Infrastructure.Data
+{
+    public class ClipUrlDbContext : DbContext
+    {
+        public ClipUrlDbContext(DbContextOptions<ClipUrlDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortUrl>()
+                .HasKey(s => s.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
