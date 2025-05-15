343f98e28b36360864930d87aa6f2c3269d68c07
jvstDani92 <radomirovDaniel@gmail.com>
2025-05-15 19:33:22 +0200

    feat(api): add urlController

---
+++ ClipUrl.Api/Controllers/UrlController.cs
@@ -0,0 +1,58 @@
+﻿using ClipUrl.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClipUrl.Api.Controllers
+{
+    [ApiController]
+    [Route("api/urls")]
+    public class UrlController : ControllerBase
+    {
+        private readonly IUrlService _urlService;
+
+        public UrlController(IUrlService urlService)
+        {
+            _urlService = urlService;
+        }
+
+        [HttpGet("{hash}")]
+        public async Task<IActionResult> GetCompleteUrl(string hash)
+        {
+            try
+            {
+                var completeUrl = await _urlService.GetOriginalUrlAsync(hash);
+
+                if (completeUrl is null)
+                    return NotFound();
+
+                return Redirect(completeUrl);
+            }
+            catch (ArgumentNullException nullEx)
+            {
+                return BadRequest(nullEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> InsertUrl([FromBody] string url)
+        {
+            try
+            {
+                var shortUrl = await _urlService.CreateShortUrlAsync(url);
+                return CreatedAtAction(nameof(GetCompleteUrl), new { hash = shortUrl }, shortUrl);
+            }
+            catch (ArgumentNullException nullEx)
+            {
+                return BadRequest(nullEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
